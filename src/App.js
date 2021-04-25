cd3ad5ba3c246b616811b6977e9445341e25aa9d
Gianfranco <gianscapin96@gmail.com>
2021-04-24 23:17:58 -0300

    agregado la lista de items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React,{Fragment} from 'react';
+import React,{Fragment, BrowserRouter,Switch,Route,useState,useEffect} from 'react';
 import NavbarComerce from './components/Navbar/NavbarComerce';
 import ItemListContainer from './components/container/ItemListContainer';
 function App() {
   return (
    <Fragment>
-     <div className="container">
-       <NavbarComerce />
-       <ItemListContainer 
-        nombre='ercmax'
-       />
-     </div>
+      <div className="container">
+        <NavbarComerce />
+            <ItemListContainer 
+              nombre='ercmax'
+            />
+      </div>
    </Fragment>
   )
 }
