825e7c6f59383415ba2c64a00fd96fb520b447a4
Gianfranco <gianscapin96@gmail.com>
2021-06-18 13:08:39 -0300

    agregado env

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+
+const {REACT_APP_APIKEY, REACT_APP_APPID} = process.env;
+
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyB6dvDA5hjVuHhZAVHG8re3ExczEnuHgns",
+    apiKey: REACT_APP_APIKEY,
     authDomain: "ercmax-project-ecommerce.firebaseapp.com",
     projectId: "ercmax-project-ecommerce",
     storageBucket: "ercmax-project-ecommerce.appspot.com",
     messagingSenderId: "570864172054",
-    appId: "1:570864172054:web:a6bbabf1f5ae98b4354937"
+    appId: REACT_APP_APPID
 });
 
 export const getFirebase = () =>app;
